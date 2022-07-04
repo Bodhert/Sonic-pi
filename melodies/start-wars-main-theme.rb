9d284a987188f7fc9804b945f09fd8522c69f1b3
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2022-07-03 20:18:55 -0500

    playing

--- melodies/start-wars-main-theme.rb
+++ melodies/start-wars-main-theme.rb
@@ -1,14 +1,28 @@
 #Start wars main theme
-use_bpm 120
 
-play :D
+#things that helped me with the notes
+# https://www.onlinemictest.com/tuners/pitch-detector/
+
+use_bpm 60
+
+play :D4
 sleep 0.5
-play :D
+play :D4
 sleep 0.5
-play :D
+play :D4
 sleep 0.5
-play :G
+play :G2, release: 2
+play :G4, release: 2
+sleep 2
+play :D5, release: 2
+play :G2, release: 2
+sleep 2
+play :C3, release: 2
+play :C5
+sleep 0.5
+play :B4
+sleep 0.5
+play :A4
+sleep 0.5
 play :G5
-sleep 0.5
-play :D
-play :G
+play :G2
