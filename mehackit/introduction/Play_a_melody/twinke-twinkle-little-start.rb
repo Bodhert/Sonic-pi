178b433d2ae3a0f394fb82cd89ed78c212e1a7ed
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2022-08-19 11:04:55 -0500

    organizing stuff, e-z song to copy

---
+++ mehackit/introduction/Play_a_melody/twinke-twinkle-little-start.rb
@@ -0,0 +1,96 @@
+##| Start wars main theme
+
+##| things that helped me with the notes
+##| https://www.onlinemictest.com/tuners/pitch-detector/
+
+
+# twinkle twinkle little start
+use_bpm 40
+
+play :C4
+sleep 0.5
+play :C4
+sleep 0.5
+play :G4
+sleep 0.5
+play :G4
+sleep 0.5
+play :A4
+sleep 0.5
+play :A4
+sleep 0.5
+play :G4, release: 1
+sleep 1
+play :F4
+sleep 0.5
+play :F4
+sleep 0.5
+play :E4
+sleep 0.5
+play :E4
+sleep 0.5
+play :D4
+sleep 0.5
+play :D4
+sleep 0.5
+play :C4, release: 1
+sleep 1
+play :G4
+sleep 0.5
+play :G4
+sleep 0.5
+play :F4
+sleep 0.5
+play :F4
+sleep 0.5
+play :E4
+sleep 0.5
+play :E4
+sleep 0.5
+play :D4, release: 1
+sleep 1
+play :G4
+sleep 0.5
+play :G4
+sleep 0.5
+play :F4
+sleep 0.5
+play :F4
+sleep 0.5
+play :E4
+sleep 0.5
+play :E4
+sleep 0.5
+play :D4, release: 1
+sleep 1
+play :C4
+sleep 0.5
+play :C4
+sleep 0.5
+play :G4
+sleep 0.5
+play :G4
+sleep 0.5
+play :A4
+sleep 0.5
+play :A4
+sleep 0.5
+play :G4, release: 1
+sleep 1
+play :F4
+sleep 0.5
+play :F4
+sleep 0.5
+play :E4
+sleep 0.5
+play :E4
+sleep 0.5
+play :D4
+sleep 0.5
+play :D4
+sleep 0.5
+play :C4, release: 1
+
+
+
+
