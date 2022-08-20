ca939762cc85dc914744f762db5c15c0746abc98
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2022-08-20 16:38:47 -0500

    cycles on the song

---
+++ mehackit/introduction/Repeat a melody/twinke-twinkle-little-start-cicles.rb
@@ -0,0 +1,113 @@
+##| # twinkle twinkle little start with cicles
+use_bpm 40
+
+2.times do
+  play :C4
+  sleep 0.5
+end
+
+2.times do
+  play :G4
+  sleep 0.5
+end
+
+2.times do
+  play :A4
+  sleep 0.5
+end
+
+play :G4, release: 1
+sleep 1
+
+2.times do
+  play :F4
+  sleep 0.5
+end
+
+2.times do
+  play :E4
+  sleep 0.5
+end
+
+2.times do
+  play :D4
+  sleep 0.5
+end
+
+play :C4, release: 1
+sleep 1
+
+2.times do
+  play :G4
+  sleep 0.5
+end
+
+2.times do
+  play :F4
+  sleep 0.5
+end
+
+2.times do
+  play :E4
+  sleep 0.5
+end
+
+play :D4, release: 1
+sleep 1
+
+2.times do
+  play :G4
+  sleep 0.5
+end
+
+2.times do
+  play :F4
+  sleep 0.5
+end
+
+2.times do
+  play :E4
+  sleep 0.5
+end
+
+play :D4, release: 1
+sleep 1
+
+2.times do
+  play :C4
+  sleep 0.5
+end
+
+2.times do
+  play :G4
+  sleep 0.5
+end
+
+2.times do
+  play :A4
+  sleep 0.5
+end
+
+play :G4, release: 1
+sleep 1
+
+2.times do
+  play :F4
+  sleep 0.5
+end
+
+2.times do
+  play :E4
+  sleep 0.5
+end
+
+2.times do
+  play :D4
+  sleep 0.5
+end
+
+play :C4, release: 1
+
+
+
+
