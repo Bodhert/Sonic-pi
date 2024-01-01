f5f00300963ed9869a7f05dc51ce513fb148e236
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2024-01-01 15:25:37 -0500

    review improvement

--- playground/osc_experimenting.rb
+++ playground/osc_experimenting.rb
@@ -1,16 +1,26 @@
-port = 54259
-use_osc "localhost", port
+host = "127.0.0.1"
+
+define :get_port do |address|
+  v = get_event(address).to_s.split(",")[6]
+  if v != nil
+    return v[3..-2].split("/")[0].split(":")[2].to_i
+  else
+    return "holi"
+  end
+end
 
 live_loop :ping do
   use_real_time
   a, b, c = sync "/osc*/ping"
-  ##| synth :prophet, note: a, cutoff: b, sustain: c
+  port = get_port("/osc*/ping")
+  puts "sending to port #{port}"
   play 60
-  osc "/pong"
+  osc_send host, port, "/pong"
 end
 
 live_loop :pong do
   use_real_time
   a, b, c = sync "/osc*/pong"
+  puts "Received #{a}, #{b}, #{c}"
   play 65
 end
