8f83a98b9eda6838c2bb7e929ce52337ca785247
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2024-12-01 00:17:25 -0500

    working through the guaracha song

--- songs/guaracha.rb
+++ songs/guaracha.rb
@@ -3,63 +3,124 @@ use_bpm 128
 samples_dir = "~/Workbench/Sonic-pi/resources/guaracha"
 load_samples samples_dir
 
+set :counter, 1
+
 live_loop :met do
-  ##| play 60
+  current = get[:counter]
+  print current
+  set :counter, current + 1
   sleep 1
 end
 
-live_loop :kick, sync: :met do
-  stop
-  sample :bd_ada
-  sleep 1
-end
 
-##| #chuck
-with_fx :compressor, threshold: 0.9 do
-  live_loop :clap, sync: :met do
-    stop
-    sample :drum_snare_hard,  hpf: 90, pre_amp: 2
-    sleep 1
+
+define :kick do |length = 4|
+  in_thread do
+    
+    length.times do
+      if get[:counter] != 16  then
+        sample :bd_ada
+        sleep 1
+      else
+        #redoble part
+        ##| sample :bd_ada
+        sample :drum_snare_hard, amp: 0.5
+        sleep 0.05
+        sample :drum_snare_hard
+        sleep 0.95
+      end
+      
+    end
+    
   end
 end
 
-
-# Perc
-drums = [0,0,0,1,0,0,2,0,0,0,0,1,0,0,2,0]
-live_loop :perc, sync: :met do
-  stop
-  drums.tick
-  sample :tabla_ke1 if drums.look == 1
-  sample :tabla_tun1, finish: 0.5 if drums.look == 2
-  sleep 0.25
+define :clap do |length = 4|
+  with_fx :compressor, threshold: 0.9 do
+    
+    in_thread do
+      length.times do
+        sample :drum_snare_hard,  hpf: 90, pre_amp: 2
+        sleep 1
+      end
+    end
+  end
 end
 
-
-
-live_loop :shaker_3, sync: :met do
-  stop
-  samba_shake = sample_paths samples_dir, "samba_shake_124"
-  puts samba_shake
-  sample samba_shake, beat_stretch: 5
-  sleep 5
+define :percusion  do |length = 16|
+  drums = [0,0,0,1,0,0,2,0,0,0,0,1,0,0,2,0]
+  in_thread do
+    (length * 4).times do
+      drums.tick
+      sample :tabla_ke1 if drums.look == 1
+      sample :tabla_tun1, finish: 0.5 if drums.look == 2
+      sleep 0.25
+    end
+  end
 end
 
-
-live_loop :vocals, sync: :met do
-  ##| stop
-  ## this can be take advantange of the onset and can be randomized
-  vocals = sample_paths samples_dir, "160743__qubodup__tribal-battle-chantsshouts"
-  sample vocals, onset: 4
-  sleep 1
+# investigate how to cut appropiatly the shaker
+define :shaker do |length = 1|
+  in_thread do
+    length.times do
+      samba_shake = sample_paths samples_dir, "samba_shake_128_chopped"
+      sample samba_shake, beat_stretch: 4
+      sleep 4
+    end
+  end
 end
 
-live_loop :redoble, sync: :met do
-  sample :drum_heavy_kick
-  sleep 0.25
-  sample :drum_snare_hard
-  sleep 0.25
-  sleep 1.5
+define :vocals do |length = 4|
+  in_thread do
+    length.times do
+      vocals = sample_paths samples_dir, "160743__qubodup__tribal-battle-chantsshouts"
+      sample vocals, onset: 4
+      sleep 1
+    end
+  end
 end
 
+with_fx :lpf, cutoff: 100, cutoff_slide: 0.5 do | lpf |
+  define :donk do |length = 16|
+    use_synth :square
+    use_synth_defaults release: 0.25
+    in_thread do
+      length.times do
+        print lpf
+        play :G2, amp: 1
+        sleep 0.5
+        play :G2 + 5, amp: 1 * 0.4
+        control lpf, cutoff: 10
+        sleep 0.5
+      end
+    end
+  end
+end
 
+# investigate how to order by time
+##| define :redoble do
+##|   in_thread do
+##|     16.times do
+##|       if get[:counter] == 16 then
+##|         sample :drum_snare_hard, amp: 0.5
+##|         sleep 0.19
+##|         sample :drum_snare_hard
+##|         sleep 0.19
+##|       end
+##|       sleep 1
+##|     end
+##|   end
+##| end
+
+
+
+
+
+##| kick(16);
+##| redoble();
+##| clap(16);
+##| percusion(16);
+##| vocals(16)
+##| shaker(4);
+donk()
 
